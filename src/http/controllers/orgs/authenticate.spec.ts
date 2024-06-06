e22dbae117b203e0a05ea7904e4ff83d801a4705
Guilhermecheng <guilherme.cheng@gmail.com>
2024-06-06 00:35:33 -0300

    feat: authenticate e2e test init

---
+++ src/http/controllers/orgs/authenticate.spec.ts
@@ -0,0 +1,44 @@
+import request from 'supertest'
+import { app } from '@/app'
+import { beforeAll, afterAll, describe, expect, it } from 'vitest'
+
+describe('Authenticate Organization (e2e)', () => {
+    beforeAll(async () => {
+        await app.ready()
+    })
+
+    afterAll(async () => {
+        await app.close()
+    })
+
+    it('should be able to authenticate an organization', async () => {
+        await request(app.server)
+            .post('/organization')
+            .send({
+                responsibleName: "Responsável 1",
+                title: "Organização Teste",
+                email: "org_teste@teste.com",
+                cep: "05040000",
+                city: "São Paulo",
+                state: "SP",
+                address: "Rua Américo Paulino 123",
+                whatsapp: "(11) 9 1234 1234",
+                password: "123456"
+            })
+
+        const response = await request(app.server)
+            .post('/sessions')
+            .send({
+                email: "org_teste@teste.com",
+                password: "123456"
+            })
+
+        expect(response.statusCode).toEqual(200)
+        // expect(response.body.message).toEqual("Organização cadastrada com sucesso!")
+    })
+
+        // it('should not be albe to authenticate a non-existent email', async () => {
+
+        // })
+
+})
